7e1c2a3ad47e0f6b8ec1d437d48cd23e0ab104c4
Xtalism <manuel_lego1234@hotmail.com>
2025-09-09 02:23:39 -0600

    Template script addition

---
+++ telegram_bot_template.py
@@ -0,0 +1,82 @@
+from typing import Final
+
+from telegram import Update
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ContextTypes,
+    MessageHandler,
+    filters,
+)
+
+TOKEN = "7654112373:AAFFJrOJh4c-Xhl50VnREPqCtRtOq2DGt98"
+
+BOT_USERNAME: Final = "@cfe_ia_bot"
+
+
+class TelegramCommands:
+    def __init__(self) -> None:
+        pass
+
+    async def start_command(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        await update.message.reply_text("CFE - Inteligencia Artificial Iniciado")
+
+    async def help_command(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        await update.message.reply_text("¿Necesitas ayuda?")
+
+    async def custom_command(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        await update.message.reply_text("Custom command")
+
+
+class HandleResponses:
+    def __init__(self) -> None:
+        pass
+
+    def handle_response(self, text: str) -> str:
+        processed: str = text.lower()
+
+        if "hello" in processed:
+            return "hey there!"
+
+        return "No entiendo qué es lo que quieres decir"
+
+    async def handle_message(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        message_type: str = update.message.chat.type
+        text: str = str(update.message.text).lower()
+
+        print(f"User ({update.message.chat.id}) in {message_type}: {text}")
+
+        if message_type == "group":
+            if BOT_USERNAME in text:
+                new_text: str = text.replace(BOT_USERNAME, "").strip()
+                response: str = self.handle_response(new_text)
+            else:
+                return
+        else:
+            response: str = self.handle_response(text)
+        print("bot:", response)
+
+        await update.message.reply_text(response)
+
+
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f"Update {update} caused error {context.error}")
+
+
+if __name__ == "__main__":
+    print("Starting bot...")
+    app = Application.builder().token(TOKEN).build()
+
+    commands = TelegramCommands()
+    responses = HandleResponses()
+
+    app.add_handler(CommandHandler("start", commands.start_command))
+    app.add_handler(CommandHandler("help", commands.help_command))
+    app.add_handler(CommandHandler("custom", commands.custom_command))
+
+    app.add_handler(MessageHandler(filters.TEXT, responses.handle_message))
+
+    app.add_error_handler(error)
+
+    print("Polling...")
+    app.run_polling(poll_interval=3)
